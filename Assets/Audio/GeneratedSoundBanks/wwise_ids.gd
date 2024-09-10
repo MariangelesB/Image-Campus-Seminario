70402e69ea73acdd9703d009ca72fceec3e10b17
Guido Fazzito <gf@dfad.us>
2024-09-10 12:38:45 -0300

    Corregido volumen música, cambiados nombres switches

--- Assets/Audio/GeneratedSoundBanks/wwise_ids.gd
+++ Assets/Audio/GeneratedSoundBanks/wwise_ids.gd
@@ -2,21 +2,25 @@ class_name AK
 
 class EVENTS:
 
-	const SWITCH_TERRENO_TIERRA = 2305370377
-	const SWITCH_SUPERFICIE_CEMENTO = 2795773837
 	const PLAY_CLICK = 311910498
-	const PLAY_MUSICA = 4237703276
+	const PLAY_SWITCH_SUPERFICIE_TIERRA = 4121056118
+	const PLAY_ESTADO_GANAR = 3200380486
 	const PLAY_COMER_HAMBURGUESA = 1680200567
+	const PLAY_MUSICA = 4237703276
+	const PLAY_SWITCH_SUPERFICIE_CEMENTO = 656393096
 	const PLAY_HOVER = 1962302600
+	const PLAY_ESTADO_GAMEPLAY = 1884797811
 	const PLAY_PASOS = 4031831520
 
 	const _dict = {
-		"Switch_Terreno_Tierra": SWITCH_TERRENO_TIERRA,
-		"Switch_Superficie_Cemento": SWITCH_SUPERFICIE_CEMENTO,
 		"Play_Click": PLAY_CLICK,
-		"Play_Musica": PLAY_MUSICA,
+		"Play_Switch_Superficie_Tierra": PLAY_SWITCH_SUPERFICIE_TIERRA,
+		"Play_Estado_Ganar": PLAY_ESTADO_GANAR,
 		"Play_Comer_Hamburguesa": PLAY_COMER_HAMBURGUESA,
+		"Play_Musica": PLAY_MUSICA,
+		"Play_Switch_Superficie_Cemento": PLAY_SWITCH_SUPERFICIE_CEMENTO,
 		"Play_Hover": PLAY_HOVER,
+		"Play_Estado_Gameplay": PLAY_ESTADO_GAMEPLAY,
 		"Play_Pasos": PLAY_PASOS
 	}
 
@@ -26,19 +30,19 @@ class STATES:
 		const GROUP = 3685021506
 
 		class STATE:
-			const GAMEPLAY = 89505537
-			const NONE = 748895195
-			const GANAR = 3575068712
 			const MENU = 2607556080
+			const GANAR = 3575068712
+			const NONE = 748895195
+			const GAMEPLAY = 89505537
 
 	const _dict = {
 		"Estado_Del_Juego": {
 			"GROUP": 3685021506,
 			"STATE": {
-				"Gameplay": 89505537,
-				"None": 748895195,
+				"Menu": 2607556080,
 				"Ganar": 3575068712,
-				"Menu": 2607556080
+				"None": 748895195,
+				"Gameplay": 89505537
 			}
 		}
 	}
@@ -49,31 +53,31 @@ class SWITCHES:
 		const GROUP = 3001233698
 
 		class SWITCH:
-			const CEMENTO = 1137103080
 			const TIERRA = 2565746006
+			const CEMENTO = 1137103080
 
 	const _dict = {
 		"Superficie": {
 			"GROUP": 3001233698,
 			"SWITCH": {
-				"Cemento": 1137103080,
-				"Tierra": 2565746006
+				"Tierra": 2565746006,
+				"Cemento": 1137103080
 			}
 		}
 	}
 
 class GAME_PARAMETERS:
 
-	const CANTIDAD_TIMESTRETCH = 2448709148
+	const SX_VOLUME = 3075796249
 	const MX_VOLUME = 1441253587
 	const DISTANCIA_HAMBURGUESA = 2185633902
-	const SX_VOLUME = 3075796249
+	const CANTIDAD_TIMESTRETCH = 2448709148
 
 	const _dict = {
-		"Cantidad_Timestretch": CANTIDAD_TIMESTRETCH,
+		"SX_Volume": SX_VOLUME,
 		"MX_Volume": MX_VOLUME,
 		"Distancia_Hamburguesa": DISTANCIA_HAMBURGUESA,
-		"SX_Volume": SX_VOLUME
+		"Cantidad_Timestretch": CANTIDAD_TIMESTRETCH
 	}
 
 class TRIGGERS:
--- Scripts/switches_areas.gd
+++ Scripts/switches_areas.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 # Enum definitions for GroundType and AmbienceType
-enum GroundType { DIRT, GRASS }
+enum GroundType { GRAVEL, DIRT }
 
 # Export variables
 @export var ground_type : GroundType
@@ -11,10 +11,10 @@ func _on_body_entered(body : Node3D):
 	if body.is_in_group("Player"):
 		match ground_type:
 	# Setear Switch si está en Tierra
-			GroundType.DIRT:	
+			GroundType.GRAVEL:	
 				Wwise.post_event("Play_Switch_Superficie_Cemento", AudioManager)
-				print("Entered Dirt Ground Area")
+				print("Entered Gravel Ground Area")
 	# Setear Switch si está en Pasto
-			GroundType.GRASS:
+			GroundType.DIRT:
 				Wwise.post_event("Play_Switch_Superficie_Tierra", AudioManager)
-				print("Entered Grass Ground Area")
+				print("Entered Dirt Ground Area")
